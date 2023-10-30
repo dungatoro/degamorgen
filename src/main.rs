787f2bbac231bc76621e93193805c27735eeec80
dungatoro <dungatoro@arch.localdomain>
2023-10-30 21:15:19 +0000

    changed struct
    got rid of BoolExpr struct for a single enum called Gate. The recursive enum structure is much easier to work with and more logical. It eliminates unused states with fields being irrelevant because another field is None.

--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,130 @@
+// #[derive(Clone)]
+// enum BoolOp {
+//     NOT,
+//     AND,
+//     OR,
+//     NONE,
+// }
+// 
+// impl BoolOp {
+//     fn to_string(&self) -> String {
+//         String::from( match self {
+//             BoolOp::NOT  => "!",
+//             BoolOp::AND  => ".",
+//             BoolOp::OR   => "+",
+//             BoolOp::NONE => "",
+//         })
+//     }
+// }
+// 
+// #[derive(Clone)]
+// struct BoolExpr {
+//     operator: BoolOp,
+//     operands: Vec<BoolExpr>,
+//     id: Option<String>
+// }
+// 
+// impl BoolExpr {
+//     fn expand_to_string(&self) -> String {
+//         match &self.id {
+//             Some(id) => format!("{}{}", self.operator.to_string(), id),
+//             None => {
+//                 let mut expanded = self.operator.to_string();
+//                 for input in &self.operands {
+//                     expanded = format!("{} {}", expanded, input.expand_to_string());
+//                 }
+//                 format!("({})", expanded)
+//             }
+//         }
+//     }
+// }
+// 
+// impl std::fmt::Display for BoolExpr {
+//     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+//         write!(f, "{}", self.expand_to_string())
+//     }
+// }
+// 
+// fn main() {
+//     
+//     let a = BoolExpr {
+//         operator: BoolOp::NONE,
+//         operands: vec![],
+//         id: Some("a".to_string())
+//     };
+// 
+//     let b = BoolExpr {
+//         operator: BoolOp::NONE,
+//         operands: vec![],
+//         id: Some("b".to_string())
+//     };
+// 
+//     let c = BoolExpr {
+//         operator: BoolOp::NOT,
+//         operands: vec![],
+//         id: Some("c".to_string())
+//     };
+// 
+//     let a_and_b = BoolExpr {
+//         operator: BoolOp::AND,
+//         operands: vec![a.clone(), b],
+//         id: None
+//     };
+// 
+//     let a_or_not_c_or_a_and_b = BoolExpr {
+//         operator: BoolOp::OR,
+//         operands: vec![a.clone(), c, a_and_b.clone()],
+//         id: None
+//     };
+// 
+//     println!("{}", a_or_not_c_or_a_and_b);
+// 
+// }
+
+
 #[derive(Clone)]
-enum BoolOp {
-    NOT,
-    AND,
-    OR,
-    NONE,
+enum Gate {
+    NOT(Box<Gate>),
+    AND(Vec<Gate>),
+    OR(Vec<Gate>),
+    ID(String)
 }
 
-impl BoolOp {
+use crate::Gate::*;
+
+impl Gate {
     fn to_string(&self) -> String {
-        String::from( match self {
-            BoolOp::NOT  => "!",
-            BoolOp::AND  => ".",
-            BoolOp::OR   => "+",
-            BoolOp::NONE => "",
-        })
-    }
-}
-
-#[derive(Clone)]
-struct BoolExpr {
-    operator: BoolOp,
-    operands: Vec<BoolExpr>,
-    id: Option<String>
-}
-
-impl BoolExpr {
-    fn expand_to_string(&self) -> String {
-        match &self.id {
-            Some(id) => format!("{}{}", self.operator.to_string(), id),
-            None => {
-                let mut expanded = self.operator.to_string();
-                for input in &self.operands {
-                    expanded = format!("{} {}", expanded, input.expand_to_string());
-                }
-                format!("({})", expanded)
+        match self {
+            Self::ID(ident) => ident.clone(),
+            Self::NOT(input) => {
+                let arg: String = input.to_string();
+                format!("!{arg}")
+            }
+            Self::AND(inputs) => {
+                let args: String = inputs
+                    .iter()
+                    .map(|ip| format!(" {}", ip.to_string()))
+                    .collect();
+                format!("(AND{args})") 
+            }
+            Self::OR(inputs) => {
+                let args: String = inputs
+                    .iter()
+                    .map(|ip| format!(" {}", ip.to_string()))
+                    .collect();
+                format!("(OR{args})") 
             }
         }
-    }
-}
 
-impl std::fmt::Display for BoolExpr {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "{}", self.expand_to_string())
     }
 }
 
 fn main() {
-    
-    let a = BoolExpr {
-        operator: BoolOp::NONE,
-        operands: vec![],
-        id: Some("a".to_string())
-    };
+    let a = ID("a".to_string());
+    let b = ID("b".to_string());
+    let c = ID("c".to_string());
 
-    let b = BoolExpr {
-        operator: BoolOp::NONE,
-        operands: vec![],
-        id: Some("b".to_string())
-    };
-
-    let c = BoolExpr {
-        operator: BoolOp::NOT,
-        operands: vec![],
-        id: Some("c".to_string())
-    };
-
-    let a_and_b = BoolExpr {
-        operator: BoolOp::AND,
-        operands: vec![a.clone(), b],
-        id: None
-    };
-
-    let a_or_not_c_or_a_and_b = BoolExpr {
-        operator: BoolOp::OR,
-        operands: vec![a.clone(), c, a_and_b.clone()],
-        id: None
-    };
-
-    println!("{}", a_or_not_c_or_a_and_b);
-
+    let expr = OR( vec![ a.clone(), NOT(Box::new(c)), AND( vec![a.clone(), b] )] );
+    println!("{}", expr.to_string());
 }
+
